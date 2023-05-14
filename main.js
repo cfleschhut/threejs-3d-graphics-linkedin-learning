ddd26fb93deb87a3389e1856e7e2cf4509f16376
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-05-14 22:58:44 +0200

    Populate scene

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  1,
-  1000
-);
+const getBox = (width, height, depth) => {
+  const geometry = new THREE.BoxGeometry(width, height, depth);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const mesh = new THREE.Mesh(geometry, material);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+  return mesh;
+};
 
-document.getElementById('webgl').append(renderer.domElement);
+const init = () => {
+  const scene = new THREE.Scene();
 
-renderer.render(scene, camera);
+  const box = getBox(1, 1, 1);
+  scene.add(box);
+
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.getElementById('webgl').append(renderer.domElement);
+
+  renderer.render(scene, camera);
+};
+
+init();
