00fb6ad6133b9211d1a210711d5e0b73333ed04a
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-05-14 17:46:55 +0200

    Create scene

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import * as THREE from 'three';
 
-console.log(THREE);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.getElementById('webgl').append(renderer.domElement);
+
+renderer.render(scene, camera);
